ab4b4f43b8a45cd89b10362cf6ec08f0b525133e
alex-karev <karev.as99@gmail.com>
2024-03-20 20:44:24 +0800

    Switch to tailwind.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,84 +1,21 @@
-import { Container, Row, Col, Form, Card, Image, ListGroup, Button, Stack } from "react-bootstrap"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faCaretLeft } from "@fortawesome/free-solid-svg-icons";
 
 function App() {
   return (
-    <Container fluid className="min-vh-100 d-flex flex-column">
-      <Row className="flex-grow-1">
-        <Col className="bg-body-tertiary shadow-border d-flex flex-column" xs={3} >
-          <Container className="flex-grow-1 d-flex flex-column">
-            <Row className="p-2">
-              <h1 className="text-center">Yallmi</h1>
-              <hr />
-            </Row>
-            <Row className="flex-grow-1">
-              <ListGroup className="p-2">
-                <ListGroup.Item action>Cras justo odio</ListGroup.Item>
-                <ListGroup.Item action>Dapibus ac facilisis in</ListGroup.Item>
-                <ListGroup.Item action active>Morbi leo risus</ListGroup.Item>
-                <ListGroup.Item action>Porta ac consectetur ac</ListGroup.Item>
-                <ListGroup.Item action>Vestibulum at eros</ListGroup.Item>
-              </ListGroup>
-            </Row>
-            <Row className="p-2">
-              <Stack direction="vertical" gap={2}>
-                <Button variant="primary">Settings</Button>
-                <Button variant="primary">About</Button>
-              </Stack>
-            </Row>
-          </Container>
-        </Col>
-        <Col className="d-flex flex-column">
-          <Row className="bg-body-tertiary shadow-border p-2 d-flex justify-content-between">
-            <Col>
-              <span>Navbar - Left</span>
-            </Col>
-            <Col className="d-flex justify-content-end">
-              <span>Navbar - Right</span>
-            </Col>
-          </Row>
-          <Row className="flex-grow-1">
-            <Container>
-              <Row className="p-2 d-flex align-items-center">
-                <Col>
-                  <Card>
-                    <Card.Body>Question...</Card.Body>
-                  </Card>
-                </Col>
-                <Col xs="auto">
-                  <Image src="logo.svg" roundedCircle style={{ width: "50px" }} />
-                </Col>
-              </Row>
-              <Row className="p-2 d-flex align-items-center">
-                <Col xs="auto">
-                  <Image src="logo.svg" roundedCircle style={{ width: "50px" }} />
-                </Col>
-                <Col>
-                  <Card border="light">
-                    <Card.Body>Answer...</Card.Body>
-                  </Card>
-                </Col>
-              </Row>
-            </Container>
-          </Row>
-          <Row className="p-3 d-flex align-items-center">
-            <Col>
-              <Form.Control type="text" as="textarea" placeholder="Type your question..." rows={5}></Form.Control>
-            </Col>
-            <Col xs="auto">
-              <Button variant="primary">Send</Button>
-            </Col>
-          </Row>
-        </Col>
-      </Row>
-      <Row className="bg-body-tertiary shadow-border p-2 d-flex justify-content-between">
-        <Col>
-          <span>Command Line - Left</span>
-        </Col>
-        <Col className="d-flex justify-content-end">
-          <span>Command Line - Right</span>
-        </Col>
-      </Row>
-    </Container>
+    <div className="h-dvh flex flex-col">
+      <div className="grow flex">
+        <div className="h-full w-full sm:w-72 lg:w-96 shadow-inner p-6 z-10">
+          <span>Sidebar</span>
+        </div>
+        <div className="hidden sm:block h-full grow shadow-inner p-6 z-0">
+          <span>Content</span>
+        </div>
+      </div>
+      <div className="shadow-inner w-full content-center p-2 z-20">
+        <span>Command Line</span>
+      </div>
+    </div>
   )
 }
 
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  darkMode: "class",
+  theme: {
+    extend: {},
+    plugins: [],
+  }
+}
