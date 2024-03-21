598671fa3f250829cbe1df7ede3ae82dcf9d4f06
alex-karev <karev.as99@gmail.com>
2024-03-21 12:41:48 +0800

    Modify layout

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,26 @@ import { faBars, faCaretLeft } from "@fortawesome/free-solid-svg-icons";
 
 function App() {
   return (
-    <div className="h-dvh flex flex-col">
-      <div className="grow flex">
-        <div className="h-full w-full sm:w-72 lg:w-96 shadow-inner p-6 z-10">
-          <span>Sidebar</span>
+    <div class="flex min-h-screen flex-col bg-gray-100 text-gray-800">
+      <div class="flex flex-row w-full py-4 pr-4 content-center items-center justify-between bg-white shadow-md z-10">
+        <div class="flex flex-row gap-x-4 w-64 justify-between">
+          <div className="flex-grow text-center">
+            <button className="font-mono text-xl font-bold"><FontAwesomeIcon icon={faBars} /></button>
+          </div>
+          <span className="font-mono text-3xl font-bold grow">Yallmi</span>
         </div>
-        <div className="hidden sm:block h-full grow shadow-inner p-6 z-0">
-          <span>Content</span>
-        </div>
+        <span className="font-mono text-xl font-bold">Navbar Right</span>
       </div>
-      <div className="shadow-inner w-full content-center p-2 z-20">
-        <span>Command Line</span>
+      <div className="flex flex-row w-full grow z-0">
+        <aside class="sidebar w-96 md:w-64 -translate-x-full transform bg-white p-4 transition-transform duration-150 ease-in md:translate-x-0 md:shadow-md">
+          <div class="my-4 w-full text-center">
+            <span class="font-mono text-xl font-bold tracking-widest">Sidebar</span>
+          </div>
+          <div class="my-4"></div>
+        </aside>
+        <main class="main -ml-96 flex flex-grow flex-col transition-all duration-150 ease-in md:ml-0">
+          <div class="flex h-full items-center justify-center bg-white text-center text-5xl font-bold shadow-md">Content</div>
+        </main>
       </div>
     </div>
   )
